00dcac3191ca9dad81a8d023e76a0415d2c2698a
Vincent Vaur <vaur.vincent@gmail.com>
2021-03-10 12:19:15 +0100

    We did it !

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,15 @@
-const fizzBuzz = () => {
+const fizzBuzz = (number) => {
   // Start here
-};
+  if(number %3 === 0 && number %5 === 0) {
+    return "fizzbuzz"
+  } 
+  else if(number %5 === 0){
+    return "buzz"
+  }
+  else if (number %3 === 0) {
+    return "fizz"
+  }
+  else {return number};
+}
 
-module.exports = fizzBuzz;
+module.exports = fizzBuzz
